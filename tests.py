47a25025dd0bb6112f974f04fe918726fd5c5da0
Jimbo <marquardtjames@gmail.com>
2016-12-15 20:41:01 -0600

    is_words test

--- tests.py
+++ tests.py
@@ -7,8 +7,15 @@ class TestWordCheck(unittest.TestCase):
     def testWoodshed(self):
         self.assertEquals(checkwords.is_word("woodshed"), True)
 
-    def dog(self):
+    def testdog(self):
         self.assertEquals(checkwords.is_word("dog"),True)
+
+    def testxxxyz(self):
+        self.assertEquals(checkwords.is_word("xxxyz"),False) 
+
+    # test multiword match
+    def testboomboom(self):
+        self.assertEquals(checkwords.is_words("boomboom"),True)
 
 
 if __name__ == "__main__":
