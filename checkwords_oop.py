208554e8b19a59f9e91c21c06abe3953e3fed4ce
Jimbo <marquardtjames@gmail.com>
2017-03-05 05:56:16 -0600

    oop files

---
+++ checkwords_oop.py
@@ -0,0 +1,45 @@
+#check words using prefix lists in algorithm somehow?
+
+from dictwords import load_words, load_prefix_lists
+
+class WordsChecker(object):
+    def __init__(self):
+        super(WordsChecker,self).__init__()
+        self.word_sets = []
+        for i in range(0,8):
+            self.word_sets.append(load_words(i+1))
+        self.prefix_lists = load_prefix_lists()
+        self.cache = {}
+
+    def is_word(self,s):
+        if s:
+            return s in self.word_sets[len(s)-1]
+        else:
+            return False
+    
+    def is_words(self,s):
+        cached = self.cache.get(s,-1)
+        if cached != -1:
+            return cached
+        i = 1
+        len_s = len(s)
+        while i <= len_s:
+            currently_checking = s[0:i]
+            if self.is_word(currently_checking):
+                if len_s==i:
+                    self.cache[s]=True
+                    return True
+                elif self.is_words(s[i:]):
+                    self.cache[s]=True
+                    return True
+            if i in self.prefix_lists and currently_checking in self.prefix_lists[i]:
+                i+=1
+            else:
+                self.cache[s]=False
+                return False
+        self.cache[s]=False
+        return False
+
+
+if __name__=="__main__":
+    pass
---
+++ scramble_finder_oop.py
@@ -0,0 +1,38 @@
+# this file is used to actually loop through starting tattoos, scramble them, and then check to see if the scrambles are composed of real words
+from checkwords_oop import WordsChecker
+from collections import deque
+import scramble
+
+# maybe make this a Class that can store the cache as self.cache
+
+class ScrambleFinder(WordsChecker):
+    def __init__(self):
+        super(ScrambleFinder,self).__init__()
+        self.input_tats = []
+        for i in range(0,4):
+            for word in self.word_sets[i]:
+                for word2 in self.word_sets[6-i]:
+                    w1 = word+word2
+                    self.input_tats.append(w1)
+                    if i!=3:
+                        w2 = word2+word
+                        self.input_tats.append(w2)
+        for word in self.word_sets[7]:
+            #add the 8 letter words
+            self.input_tats.append(word)
+        #get rid of duplicates
+        self.input_tats = deque(set(self.input_tats))
+    
+    def checkScrambles(self):
+        while(self.input_tats):
+            self.checkScramble(self.input_tats.pop())
+
+    def checkScramble(self,tat):
+        scrambles = scramble.scramble(tat)
+        for scrambled_tat in scrambles:
+            if self.is_words(scrambled_tat):
+                print "%s -> %s" % (tat,scrambled_tat)
+
+if __name__ == "__main__":
+    scramble_finder = ScrambleFinder()
+    scramble_finder.checkScrambles()
